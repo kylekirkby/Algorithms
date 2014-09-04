6e9b401aa8ecf2b1b40c08f9dd73ff6d27d35dd9
Kyle Kirkby <30710@longroad.ac.uk>
2014-09-04 11:58:32 +0100

    Added is_empty method.

--- stack.py
+++ stack.py
@@ -34,28 +34,42 @@ class Stack:
         size = self.size()
         if size != 0:
             self.items.pop()
+            self.stack_pointer = size
             return True
         else:
             print("Stack is empty.")
             return False
 
     def peek(self):
-
-        lastItem = self.items[-1]
-
-        return lastItem
+        empty = self.is_empty()
+        if empty == True:
+            print("Stack is empty.")
+        else:    
+            lastItem = self.items[-1]
+            return lastItem
             
             
-        
+    def is_empty(self):
+
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
         
 
 def main():
     stack = Stack(5)
-    stack.push(1)
-    stack.push(2)
     print(stack.items)
     print(stack.stack_pointer)
     print(stack.peek())
+    for i in range(1,10):
+        stack.push(i)
+        print(stack.items)
+    for i in range(1,10):
+        stack.pop()
+
+    print(stack.is_empty())
+        
 
 if __name__ == "__main__":
 
